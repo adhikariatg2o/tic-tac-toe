1b855ce5890717364c28c8e28033a736a0a2b136
Ashok Adhikari <aadhikari@g2o.com>
2021-11-08 11:54:57 -0500

    Test Case 3 - Implemented

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ function App() {
   return (
     <div className="App">
       <h1 className="title">TIC TAC TOE</h1>
-      <div className="game-board"></div>
+      <div className="game-board">
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+      </div>
     </div>
   );
 }
